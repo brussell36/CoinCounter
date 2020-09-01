fd449a2034e25016fcd3a602d54e579ddc65ca03
Ben Russell <benjaminrussell36@gmail.com>
2020-08-31 18:30:43 -0700

    Add test for recursive function.
    Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- __tests__/coin-counter.test.js
+++ __tests__/coin-counter.test.js
@@ -1,5 +1,7 @@
 import { amountToCoins } from './../src/coin-counter.js';
 
+// describe("amountToCoins", () => {
+
 test('verifies that amountToCoins returns quarters', function() {
   expect(amountToCoins(46, [25, 10, 5, 1])).toEqual([25, 10, 10, 1]);
 });
